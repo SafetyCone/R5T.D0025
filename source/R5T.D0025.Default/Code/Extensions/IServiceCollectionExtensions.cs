3c4ed21e08549ade03c277d14a5967a0395d1945
MinexAutomation <minex.automation@gmail.com>
2020-07-03 01:27:08 -0700

    Added R5T.D0025.Default library. Added OSPlatformSwitch service implementation class type. Added R5T.D0024 project-reference dependency.

---
+++ source/R5T.D0025.Default/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.D0025.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddOSPlatformSwitch
+    }
+}
---
+++ source/R5T.D0025.Default/Code/Services/Implementations/OSPlatformSwitch.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+using D0024Utilities = R5T.D0024.Utilities;
+
+
+namespace R5T.D0025.Default
+{
+    public class OSPlatformSwitch : IOSPlatformSwitch
+    {
+        public void Switch(OSPlatform oSPlatform, Action windowsAction, Action osxAction, Action linuxAction)
+        {
+            if(oSPlatform == OSPlatform.Windows)
+            {
+                windowsAction();
+
+                return;
+            }
+
+            if(oSPlatform == OSPlatform.OSX)
+            {
+                osxAction();
+
+                return;
+            }
+
+            if(oSPlatform == OSPlatform.Linux)
+            {
+                linuxAction();
+
+                return;
+            }
+
+            throw D0024Utilities.GetUnknownOSPlatformException();
+        }
+
+        public Task SwitchAsync(OSPlatform oSPlatform, Func<Task> windowsActionAsync, Func<Task> osxActionAsync, Func<Task> linuxActionAsync)
+        {
+            if (oSPlatform == OSPlatform.Windows)
+            {
+                var task = windowsActionAsync();
+
+                return task;
+            }
+
+            if (oSPlatform == OSPlatform.OSX)
+            {
+                var task = osxActionAsync();
+
+                return task;
+            }
+
+            if (oSPlatform == OSPlatform.Linux)
+            {
+                var task = linuxActionAsync();
+
+                return task;
+            }
+
+            throw D0024Utilities.GetUnknownOSPlatformException();
+        }
+    }
+}
