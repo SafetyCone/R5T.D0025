a23fdc13e1c8e2b0205913155119693927df1cf4
MinexAutomation <minex.automation@gmail.com>
2020-07-03 01:06:12 -0700

    Added R5T.D0025.Base library. Added IOSPlatformSwitch service.

---
+++ source/R5T.D0025.Base/Code/Services/Definitions/IOSPlatformSwitch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0025
+{
+    public interface IOSPlatformSwitch
+    {
+        void Switch(OSPlatform oSPlatform, Action windowsAction, Action osxAction, Action linuxAction);
+        Task SwitchAsync(OSPlatform oSPlatform, Func<Task> windowsActionAsync, Func<Task> osxActionAsync, Func<Task> linuxActionAsync);
+    }
+}
---
+++ source/R5T.D0025.Base/Code/Services/Extensions/IOSPlatformSwitchExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0025
+{
+    public static class IOSPlatformSwitchExtensions
+    {
+        public static T Switch<T>(this IOSPlatformSwitch oSPlatformSwitch, OSPlatform oSPlatform, Func<T> windowsFunc, Func<T> osxFunc, Func<T> linuxFunc)
+        {
+            var output = default(T);
+
+            void WindowsAction()
+            {
+                output = windowsFunc();
+            }
+
+            void OsxAction()
+            {
+                output = osxFunc();
+            }
+
+            void LinuxAction()
+            {
+                output = linuxFunc();
+            }
+
+            oSPlatformSwitch.Switch(oSPlatform, WindowsAction, OsxAction, LinuxAction);
+
+            return output;
+        }
+
+        public static Task<T> SwitchAsync<T>(this IOSPlatformSwitch oSPlatformSwitch, OSPlatform oSPlatform, Func<Task<T>> windowsFuncAsync, Func<Task<T>> osxFuncAsync, Func<Task<T>> linuxFuncAsync)
+        {
+            Task<T> output = default;
+
+            Task WindowsActionAsync()
+            {
+                output = windowsFuncAsync();
+
+                return Task.CompletedTask;
+            }
+
+            Task OsxActionAsync()
+            {
+                output = osxFuncAsync();
+
+                return Task.CompletedTask;
+            }
+
+            Task LinuxActionAsync()
+            {
+                output = linuxFuncAsync();
+
+                return Task.CompletedTask;
+            }
+
+            oSPlatformSwitch.Switch(oSPlatform, WindowsActionAsync, OsxActionAsync, LinuxActionAsync); // Synchronous.
+
+            return output;
+        }
+    }
+}
